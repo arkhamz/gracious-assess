3dbb08f06b9b3b721e9f1971f0ad96e6878fcd8f
arkhamz <shile2007@gmail.com>
2022-10-21 12:57:35 +0200

    turned char card on episode detail into link to char page

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import "./CharacterCard.css"
+import { Link } from "react-router-dom";
 
 export default function CharacterCard({char}){
 
     return (
         <>
         {char && (
-            <article className="char-container">
-            <div className="top">
-                <img src={char.image} alt="character card"/>
-            </div>
-            <div className="bottom">
-                <p style={{color: "black", fontFamily:"wubba"}} className="char-name"> {char.name}</p>
-                <p>{char.status}</p>
-            </div>
-        </article>
+            <Link style={{textDecoration: "none"}} to={`/characters/${char.id}`}>
+                <article className="char-container">
+                    <div className="top">
+                        <img src={char.image} alt="character card"/>
+                    </div>
+                    <div className="bottom">
+                        <p style={{color: "black", fontFamily:"wubba"}} className="char-name"> {char.name}</p>
+                        <p>{char.status}</p>
+                    </div>
+                </article>
+            </Link>
         )}</>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,11 @@
-import { useEffect, useState } from "react"
-import axios from "axios";
-import Location from "../components/Location";
-import useGetLocation from "../hooks/useGetLocation";
-import useGetByDimension from "../hooks/useGetByDimension";
-import LocationPreview from "../components/LocationPreview";
 import LocationSearch from "../components/LocationSearch";
 import DimensionSearch from "../components/DimensionSearch";
-
+import "./Home.css";
 
 export default function Home(){
 
-    // array of location objects
-    // const [locationTerm,setLocationTerm] = useState("");
-    // const [dimensionTerm,setDimensionTerm] = useState("");
-    // const {location,error,getLocation} = useGetLocation();
-    // const {location:dimension,error:dimensionError,getByDimension} = useGetByDimension();
-    
-    // function handleSubmitLocation(searchTerm){
-    //     getLocation(searchTerm.toLowerCase());
-    // }
-
-    // function handleSubmitDimension(searchTerm){
-    //     getByDimension(searchTerm.toLowerCase());
-    // }
-
-    // console.log(location);
-
-
-
    return (
-    <section className="home">
+    <section className="home-container">
         <h2>Home</h2>
         <div className="home-filters">
             <LocationSearch/>
