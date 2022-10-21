9ad6a2487e54fad3485c4aa7a3c5c79771591011
arkhamz <shile2007@gmail.com>
2022-10-21 11:53:16 +0200

    pagination for episodes done

--- src/hooks/useGetEpisodes.js
+++ src/hooks/useGetEpisodes.js
@@ -5,18 +5,21 @@ export default function useGetEpisodes(){
 
     const [episodes, setEpisodes] = useState(null);
     const [error, setError] = useState(null);
+    const [maxPages,setMaxPages] = useState(null);
 
-    async function getEpisodes(){
+    async function getEpisodes(page){
 
         setError(null);
         try {
-            const response = await axios.get(`https://rickandmortyapi.com/api/episode`);
+            const response = await axios.get(`https://rickandmortyapi.com/api/episode?page=${page}`);
             const fetchedEpisodes = response.data.results;
+            const pages = response.data.info.pages
 
             if(!fetchedEpisodes){
                 throw new Error("No locations exist");
             } else{
                 setEpisodes(fetchedEpisodes);
+                setMaxPages(pages);
                 setError(null);
             }
             
@@ -27,7 +30,7 @@ export default function useGetEpisodes(){
         }
     }
 
-    return {episodes,error,getEpisodes}
+    return {episodes,error,getEpisodes, maxPages}
 
 
 }
--- src/pages/Episodes.js
+++ src/pages/Episodes.js
@@ -1,17 +1,45 @@
 import { useState, useEffect } from "react";
 import useGetEpisodes from "../hooks/useGetEpisodes";
-
+import "./Episodes.css"
 
 export default function Episodes(){
 
-    const {episodes,error,getEpisodes} = useGetEpisodes();
+    const {episodes,error,getEpisodes, maxPages} = useGetEpisodes();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage,setPostsPerPage] = useState(10);
 
     useEffect(function(){
 
-        getEpisodes();
-    },[]);
+        getEpisodes(currentPage);
+    },[currentPage]);
 
-    console.log(episodes);
+    function handleClick(direction){
+        if(direction === "next"){
+            setCurrentPage(p => p + 1);
+        } else if (direction === "previous"){
+            setCurrentPage(p => p - 1);
+        }
+    }
+
+    console.log(maxPages)
+
+
+    return (
+        <section className="episodes">
+            <ul className="episode-list">
+                {episodes && episodes.map(function(episode){
+                    return <article key={episode.id} className="episode-prev">
+                        <h4>{episode.name}</h4>
+                        <p>{episode.episode}</p>
+                    </article>
+                })}
+            </ul>
+            <div className="pagination-buttons">
+                    {currentPage > 1 && <button onClick={e => handleClick("previous") }>Previous</button>}
+                    { currentPage < maxPages && <button onClick={e => handleClick("next") }>Next</button>}
+                </div>
+        </section>
+    )
 
     
 
